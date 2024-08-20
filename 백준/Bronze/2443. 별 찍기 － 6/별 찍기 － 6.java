f14733d53f5dad50d033f702fb30460098352bbc
Leeyongseong <119646329+ukal5006@users.noreply.github.com>
2024-08-20 14:41:38 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 424 ms, Memory: 22516 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = N; i > 0; i--) {
+            for(int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+}
